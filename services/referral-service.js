90f5fd3e9d040617b8526415e3ee475931ba8fb9
monosniper <vanteateam@gmail.com>
2022-05-13 00:44:56 +0500

    fix ref token rate add

--- services/referral-service.js
+++ services/referral-service.js
@@ -12,11 +12,11 @@ class ReferralService {
             });
 
             const parent_user = await UserModel.findOne({ref_code});
-            const token_rate = parseFloat(parent_user.token_rate / 1000).toFixed(4);
-            const ref_token_rate = parseFloat(process.env.REF_REGARD_TOKEN_RATE).toFixed(4);
+            const token_rate = parseFloat(parent_user.token_rate / 1000);
+            const ref_token_rate = parseFloat(process.env.REF_REGARD_TOKEN_RATE);
 
             parent_user.refs.push(ref)
-            parent_user.token_rate = (token_rate + ref_token_rate) * 1000
+            parent_user.token_rate = parseFloat((token_rate + ref_token_rate) * 1000).toFixed(4);
             parent_user.save({ validateModifiedOnly: true })
 
             return new ReferralDto(ref);
